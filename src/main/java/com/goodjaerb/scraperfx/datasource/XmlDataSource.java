ca97b2158f7691c598052116ec0a0b2e78e3aa55
goodjaerb <goodjaerb@gmail.com>
2018-12-21 01:31:57 -0500

    have to catch this runtime exception or else a bad return from the xml will simply halt the process.

--- src/main/java/com/goodjaerb/scraperfx/datasource/XmlDataSource.java
+++ src/main/java/com/goodjaerb/scraperfx/datasource/XmlDataSource.java
@@ -7,7 +7,10 @@ package com.goodjaerb.scraperfx.datasource;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.xmappr.Xmappr;
+import org.xmappr.XmapprException;
 
 /**
  *
@@ -18,7 +21,12 @@ public abstract class XmlDataSource extends HttpDataSource {
     protected <T> T getXml(Class<T> dataHolderClass, String url) throws IOException {
         try(BufferedReader reader = getReader(url)) {
             if(reader != null) {
-                return dataHolderClass.cast(new Xmappr(dataHolderClass).fromXML(reader));
+                try {
+                    return dataHolderClass.cast(new Xmappr(dataHolderClass).fromXML(reader));
+                }
+                catch(XmapprException ex) {
+                    Logger.getLogger(XmlDataSource.class.getName()).log(Level.WARNING, "Unable to parse XML.", ex);
+                }
             }
         }
         return null;
--- src/main/java/com/goodjaerb/scraperfx/datasource/impl/ScreenScraperSource.java
+++ src/main/java/com/goodjaerb/scraperfx/datasource/impl/ScreenScraperSource.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.xmappr.XmapprException;
 
 /**
  *
